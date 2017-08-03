e1373f0ce489a64b71e0538d83edd6bdd77e9b79
Giordon Stark <kratsg@gmail.com>
2017-08-03 01:25:36 -0500

    broken code but i guess it's a start, maybe

--- train_pileup.py
+++ train_pileup.py
@@ -24,10 +24,43 @@ eta_mask = np.fabs(maps['gTowerEta'][0]) <= 2.4
 #  where e=eta, p=phi
 data = rnp.root2array(filename, treename='mytree', branches=['eventNumber', 'gTowerEt', 'Kt4EMTopoEventShape_Density'], start=0, stop=1)
 
-data = rnp.root2array(filename, treename=treename, branches=['eventNumber', ('gTowerEt',0,n_gTowers), 'Kt4EMTopoEventShape_Density'], start=0, stop=1)
+data = rnp.root2array(filename, treename=treename, branches=['eventNumber', ('gTowerEt',0,n_gTowers), 'Kt4EMTopoEventShape_Density'])
 
 # 24 eta rows and 32 phi columns
 gTowerEt = data['gTowerEt'][:, np.where(eta_mask)].reshape(-1, 24, 32)
 offline_rho = data['Kt4EMTopoEventShape_Density']
 
-import pdb; pdb.set_trace()
+''' Start training '''
+N_max = 200
+N_train = 150
+
+Train_Sample=gTowerEt[:N_train]
+Test_Sample=gTowerEt[N_train:N_max]
+
+X_Train=gTowerEt[:N_train]
+y_Train=offline_rho[:N_train]
+
+X_Test=gTowerEt[:N_train]
+y_Test=offline_rho[N_train:N_max]
+
+import os
+os.environ['THEANO_FLAGS'] = "mode=FAST_RUN,device=cuda,force_device=True"
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(12, input_dim=X_Train.shape[1], init='uniform', activation='relu'))
+model.add(Dense(8, init='uniform', activation='relu'))
+model.add(Dense(1, init='uniform', activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+history=model.fit(X_Train, y_Train, validation_data=(X_Test,y_Test), nb_epoch=10, batch_size=2048)
+
+print history.history
+
+loss_history=history.history["loss"]
+
+print loss_history
